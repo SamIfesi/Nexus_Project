5c173ebf922f10566b9f904ef8eb1b8000673afb
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2025-10-15 12:39:14 +0100

    add the schedule section and the card for HTML and the styles and intigrated the box model for the ticket in the JS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 const moveBg = document.querySelectorAll(".day");
+const model = document.querySelector(".model");
+const tickets = document.querySelectorAll(".ticket");
+const modelContainer = document.getElementById("modelContainer");
+const close = document.getElementById("close");
 
 moveBg.forEach((mover) => {
   mover.addEventListener("click", (e) => {
@@ -6,4 +10,21 @@ moveBg.forEach((mover) => {
     moveBg.forEach((removeMoveBg) => removeMoveBg.classList.remove("move"));
     mover.classList.add("move");
   });
+});
+
+tickets.forEach((ticket) => {
+  ticket.addEventListener("click", () => {
+    modelContainer.classList.add("show");
+    setTimeout(() => {
+      model.classList.add("open");
+      document.body.style.overflow = "hidden";
+    }, 500);
+  });
+});
+close.addEventListener("click", () => {
+  model.classList.remove("open");
+  setTimeout(() => {
+    document.body.style.overflow = "";
+    modelContainer.classList.remove("show");
+  }, 1500);
 });
