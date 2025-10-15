a1fd754ef7b23c5c870471026219328845cea32c
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2025-10-15 19:01:57 +0100

    Complete desktop mode

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const moveBg = document.querySelectorAll(".day");
 const model = document.querySelector(".model");
+const faqsReply = document.querySelectorAll(".ansQ");
 const tickets = document.querySelectorAll(".ticket");
+const answers = document.querySelectorAll(".ans");
+const arrows = document.querySelectorAll(".arrows");
 const modelContainer = document.getElementById("modelContainer");
 const close = document.getElementById("close");
 
@@ -27,4 +30,11 @@ close.addEventListener("click", () => {
     document.body.style.overflow = "";
     modelContainer.classList.remove("show");
   }, 1500);
+});
+answers.forEach((answer, index) => {
+  answer.addEventListener("click", () => {
+    console.log("hello");
+    faqsReply[index].classList.toggle("active");
+    arrows[index].classList.toggle("rotate");
+  });
 });
